b12f35e1a2fed75cbd913d2e3a77f9321ec9257e
irizqy <arizqyakbar@gmail.com>
2024-03-25 22:47:57 +0700

    fixed header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -32,45 +32,47 @@ const Header = () => {
 
   return (
     <header className='relative mb-[101px]'>
-      <div className='fixed top-0 left-0 right-0 z-50 bg-white shadow-sm  container mx-auto border-b border-[#f5f5f5] py-8 flex justify-between items-center'>
-        <h3 className='text-[24px] font-semibold cursor-pointer hover:opacity-60' onClick={() => navigateToPages('/')}>Ilham R</h3>
-        <nav>
-          <div className='md:hidden'>
-          <Popover>
-            <PopoverTrigger>
-              <FontAwesomeIcon className='text-[24px]' icon={faBars}/>
-            </PopoverTrigger>
-            <PopoverContent className='bg-white'>
-              <ul className='flex flex-col'>
+      <div className='fixed top-0 left-0 right-0 z-50 bg-white shadow-sm py-8 border-b border-[#f5f5f5]'>
+        <div className='container mx-auto flex justify-between items-center'>
+          <h3 className='text-[24px] font-semibold cursor-pointer hover:opacity-60' onClick={() => navigateToPages('/')}>Ilham R</h3>
+          <nav>
+            <div className='md:hidden'>
+            <Popover>
+              <PopoverTrigger>
+                <FontAwesomeIcon className='text-[24px]' icon={faBars}/>
+              </PopoverTrigger>
+              <PopoverContent className='bg-white'>
+                <ul className='flex flex-col'>
+                  {navItems.map((it, index) => {
+                    return (
+                    <li 
+                      key={index} 
+                      className='p-1 gap-2'
+                      onClick={() => navigateToPages(it.url!)}
+                      >
+                      {it.label}
+                    </li>
+                    )
+                  })}
+                </ul>
+              </PopoverContent>
+            </Popover>
+            </div>
+            <ul className='hidden md:flex gap-8'>
                 {navItems.map((it, index) => {
                   return (
                   <li 
                     key={index} 
-                    className='p-1 gap-2'
+                    className='cursor-pointer hover:opacity-60'
                     onClick={() => navigateToPages(it.url!)}
                     >
                     {it.label}
                   </li>
                   )
                 })}
-              </ul>
-            </PopoverContent>
-          </Popover>
-          </div>
-          <ul className='hidden md:flex gap-8'>
-              {navItems.map((it, index) => {
-                return (
-                <li 
-                  key={index} 
-                  className='cursor-pointer hover:opacity-60'
-                  onClick={() => navigateToPages(it.url!)}
-                  >
-                  {it.label}
-                </li>
-                )
-              })}
-          </ul>
-        </nav>
+            </ul>
+          </nav>
+        </div>
       </div>
     </header>
   )
